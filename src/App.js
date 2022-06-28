457ca239342326da1b46691b170d3b9df9eedb14
Liviu Gheorghe <liviu.gheorghe@ivfuture.uk>
2022-06-29 00:21:32 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useAuth0 } from "@auth0/auth0-react";
+
 
 function App() {
+
+  const { loginWithRedirect } = useAuth0();
+  const { logout } = useAuth0();
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <button onClick={() => loginWithRedirect()}>Log In</button>
+        <button onClick={() => logout({ returnTo: window.location.origin })}>
+        Log Out
+        </button>
+
+        <Profile />
+
     </div>
   );
 }
+
+const Profile = () => {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  return (
+    isAuthenticated && (
+      <div>
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+      </div>
+    )
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+
+
 root.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-7xee2v3w.eu.auth0.com"
+    clientId="kcYV7IUYxyyRqq8ARB5jtSPKwz51qwfm"
+    redirectUri={window.location.origin}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
